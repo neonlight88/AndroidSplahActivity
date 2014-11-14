5075e17a2574b117770d1e881b5b1f6e3bf6e46f
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2014-11-14 09:18:14 +0100

    Commit 5: New activity: SplashActivity.

--- gen/com/example/androidsplashactivity/R.java
+++ gen/com/example/androidsplashactivity/R.java
@@ -1183,7 +1183,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_splash=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1228,6 +1229,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_splash=0x7f0a000f;
     }
     public static final class style {
         /** 
